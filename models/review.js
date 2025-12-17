065d069a4ed12a6e4613858bbc2c0ba6e200c273
Neelachalamdasari <neelachalamdasari33@gmail.com>
2025-12-17 23:52:02 +0530

    Update review.js

--- models/review.js
+++ models/review.js
@@ -16,23 +16,3 @@ const reviewSchema=new Schema({
 })
 const Review=mongoose.model("Review",reviewSchema);
 module.exports = Review;
-/*const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const reviewSchema = new Schema({
-
-    comment : String,
-    rating : {
-        type : Number,
-        min : 1,
-        max : 5,
-    },
-     author:{
-        type:Schema.Types.ObjectId,
-        ref:"User",
-    },
-    createdAt : {
-        type : Date,
-        default : Date.now,
-    },
-});
-module.exports = mongoose.model("Review",reviewSchema);*/
